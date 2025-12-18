75d389be88061c08f5a381e6162cb0b9904e80a3
9GaGuLa9 <avkaras87@gmail.com>
2025-12-18 19:39:17 +0200

    update7

--- modules__init__.py
+++ modules__init__.py
@@ -4,7 +4,7 @@ Modules package для Furniture Repricer
 
 from .logger import get_logger, setup_logging, LogBlock
 from .google_sheets import GoogleSheetsClient, RepricerSheetsManager
-# from .telegram_bot import TelegramNotifier, TelegramNotifierManager
+from .telegram_bot import TelegramNotifier, TelegramNotifierManager
 from .pricing import PricingEngine, BatchPricingProcessor
 from .sku_matcher import SKUMatcher
 from .config import get_config, Config
